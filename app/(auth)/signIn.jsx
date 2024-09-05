4f7debeea4aa823795c45acd61cbc71360bfcc85
Rahul Mijar <rahulmijar0243@gmail.com>
2024-09-06 00:45:05 +0545

    implement authentication with appwrite

--- app/(auth)/signIn.jsx
+++ app/(auth)/signIn.jsx
@@ -1,17 +1,33 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { signIn } from "../../lib/appwrite";
 const SignIn = () => {
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "All fields are required");
+    }
+    setIsSubmitting(true);
+    try {
+      const response = await signIn(form.email, form.password);
+
+      // set signed user to the global state
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
--- app/(auth)/signUp.jsx
+++ app/(auth)/signUp.jsx
@@ -1,10 +1,11 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 const SignUp = () => {
   const [form, setForm] = useState({
     username: "",
@@ -13,7 +14,26 @@ const SignUp = () => {
   });
   // com.rahul.aora
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "All fields are required");
+    }
+    setIsSubmitting(true);
+    try {
+      const response = await createUser(
+        form.email,
+        form.username,
+        form.password
+      );
+
+      // set signed user to the global state
+      router.push("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
