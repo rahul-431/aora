bf9d8dd60304ffe172448a9fc3fdaf8778dc8f0b
Rahul Mijar <rahulmijar0243@gmail.com>
2024-09-05 21:02:06 +0545

    implement tabs to navigate in pages

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,104 @@
+import { View, Text, Image } from "react-native";
+import { icons } from "../../constants";
+import { Tabs, Redirect } from "expo-router";
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMethod="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "#FFA001",
+          tabBarInactiveTintColor: "#CDCDE0",
+          tabBarStyle: {
+            backgroundColor: "#161622",
+            borderTopWidth: 1,
+            borderTopColor: "#233533",
+            height: 84,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                focused={focused}
+                name="Home"
+                icon={icons.home}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                focused={focused}
+                name="Profile"
+                icon={icons.profile}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            title: "Create",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                focused={focused}
+                name="Create"
+                icon={icons.plus}
+              />
+            ),
+          }}
+        />
+
+        <Tabs.Screen
+          name="bookmark"
+          options={{
+            title: "Bookmark",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                color={color}
+                focused={focused}
+                name="Saved"
+                icon={icons.bookmark}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
--- app/index.jsx
+++ app/index.jsx
@@ -6,10 +6,10 @@ import { StatusBar } from "expo-status-bar";
 const Index = () => {
   return (
     <View className="flex flex-1 items-center justify-center">
-      <Text className="text-2xl">Welcome to the index page</Text>
+      <Text className="text-2xl font-black">Welcome to the index page</Text>
       <Text className="text-2xl">Welcome to the index page</Text>
       <StatusBar style="auto" />
-      <Link href="/profile">Go to profile</Link>
+      <Link href="/home">Go to Home</Link>
     </View>
   );
 };
