feb14e225345596637a91fc9efccbba1e465c58f
Rahul Mijar <rahulmijar0243@gmail.com>
2024-09-08 17:20:42 +0545

    implement video card with dummy data

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,65 +1,77 @@
-import { View, Text, FlatList, Image, RefreshControl } from "react-native";
-import React, { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  RefreshControl,
+  Alert,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
+import { StatusBar } from "expo-status-bar";
+import { useAppwrite } from "../../lib/useAppwrite";
+import { getAllPosts } from "../../lib/appwrite";
+import VideoCard from "../../components/VideoCard";
 const Home = () => {
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
   const [refreshing, setRefreshing] = useState(false);
   const onRefresh = async () => {
     setRefreshing(true);
-    //recall videos -> if any new videos appeard
+    await refetch();
     setRefreshing(false);
   };
   return (
-    <SafeAreaView className="bg-primary  h-full">
-      <FlatList
-        data={[{ id: 1 }, { id: 3 }, { id: 2 }]}
-        // data={[]}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
-        )}
-        ListHeaderComponent={() => (
-          <View className="my-6 px-4 space-y-6">
-            <View className="justify-between items-start flex-row mb-6">
-              <View>
-                <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back
-                </Text>
-                <Text className="text-2xl font-psemibold text-gray-100">
-                  Rahul
-                </Text>
+    <>
+      <SafeAreaView className="bg-primary  h-full">
+        <FlatList
+          data={posts ?? []}
+          keyExtractor={(item) => item.$id}
+          renderItem={({ item }) => <VideoCard post={item} />}
+          ListHeaderComponent={() => (
+            <View className="my-6 px-4 space-y-6">
+              <View className="justify-between items-start flex-row mb-6">
+                <View>
+                  <Text className="font-pmedium text-sm text-gray-100">
+                    Welcome Back
+                  </Text>
+                  <Text className="text-2xl font-psemibold text-gray-100">
+                    Rahul
+                  </Text>
+                </View>
+                <View className="mt-1.5">
+                  <Image
+                    source={images.logoSmall}
+                    className="w-9 h-10"
+                    resizeMode="contain"
+                  />
+                </View>
               </View>
-              <View className="mt-1.5">
-                <Image
-                  source={images.logoSmall}
-                  className="w-9 h-10"
-                  resizeMode="contain"
+              <SearchInput />
+              <View className="w-full flex-1 pt-5 pb-8">
+                <Text className="text-gray-100 text-lg font-pregular mb-3">
+                  Latest Videos
+                </Text>
+                <Trending
+                  posts={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] ?? []}
                 />
               </View>
             </View>
-            <SearchInput />
-            <View className="w-full flex-1 pt-5 pb-8">
-              <Text className="text-gray-100 text-lg font-pregular mb-3">
-                Latest Videos
-              </Text>
-              <Trending
-                posts={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }] ?? []}
-              />
-            </View>
-          </View>
-        )}
-        ListEmptyComponent={() => (
-          <EmptyState
-            title="No Videos found"
-            subtitle="Be the first one to upload a video"
-          />
-        ) }
-        refreshControl={<RefreshControl />}
-      />
-    </SafeAreaView>
+          )}
+          ListEmptyComponent={() => (
+            <EmptyState
+              title="No Videos found"
+              subtitle="Be the first one to upload a video"
+            />
+          )}
+          refreshControl={<RefreshControl />}
+        />
+      </SafeAreaView>
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
   );
 };
 
